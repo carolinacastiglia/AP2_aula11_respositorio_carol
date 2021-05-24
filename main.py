a869d31d8eb0b7bb59049abc8d3a40343b021b10
carolinacastiglia <carol_castiglia@hotmail.com>
2021-05-24 17:21:13 -0300

    Create main.py
    criação classe main

---
+++ main.py
@@ -0,0 +1,12 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.adicionar('C')
+lista.adicionar('A')
+lista.adicionar('R')
+lista.adicionar('O')
+lista.adicionar('L')
+
+lista.imprimir()
+lista.imprimirDoFim()
